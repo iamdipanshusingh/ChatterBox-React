d8a63f566af2ed4a49009053f393fd5ed20d37f9
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2021-04-05 01:12:12 +0530

    adds style for chat room

--- src/components/ChatRoom/Message/Message.js
+++ src/components/ChatRoom/Message/Message.js
@@ -1,7 +1,15 @@
+import classes from './Message.module.scss';
+
 function Message(props) {
-    return (<>
-        {props.message.text}
-    </>);
+    let styles = [classes.MessageDiv];
+
+    if (props.sent) {
+        styles = [...styles, classes.Sent].join(' ');
+    }
+
+    return (<div className={styles}>
+        {props.message.text.trim() !== '' ? <p>{props.message.text.trim()}</p> : null}
+    </div>);
 }
 
 export default Message;
