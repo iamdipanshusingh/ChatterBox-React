d76ad305ff2a9f14a68ee6abc48791f343e92eed
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2021-04-05 23:17:53 +0530

    adds dummy chat list component

---
+++ src/components/ChatListComponent/ChatListComponent.js
@@ -0,0 +1,11 @@
+import classes from './ChatListComponent.module.scss';
+
+const chatListComponent = props => {
+    return(
+        <div className={classes.ChatListWrapper}>
+            chat list component
+        </div>
+    );
+}
+
+export default chatListComponent;
---
+++ src/container/ChatRoomContainer/ChatRoomContainer.js
@@ -0,0 +1,35 @@
+import ChatRoom from '../../components/ChatRoom/ChatRoom';
+import ChatListComponent from '../../components/ChatListComponent/ChatListComponent';
+import classes from './ChatRoomContainer.module.scss';
+import { useState, useEffect } from 'react';
+
+function ChatRoomContainer (props) {
+    const [width, setWidth] = useState(0);
+
+    useEffect(() => {
+        const resizeEventListener = () => {
+            const width = window.innerWidth 
+            || document.documentElement.clientWidth 
+            || document.body.clientWidth;
+
+            setWidth(width);
+        }
+
+        resizeEventListener();
+
+        window.addEventListener('resize', resizeEventListener);
+
+        return () => {
+            window.removeEventListener('resize', resizeEventListener);
+        }
+    }, []);
+
+    return(
+        <div className={classes.ChatRoomContainerWrapper}>
+            {width > 900 && <ChatListComponent/>}
+            <ChatRoom/>
+        </div>
+    );
+}
+
+export default ChatRoomContainer;
