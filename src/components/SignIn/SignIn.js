9fa6bd4c011927a93285a7ea397382ba5d0d646d
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2021-04-05 17:49:59 +0530

    implements posting user's data in firestore

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -3,8 +3,21 @@ import firebase from 'firebase/app';
 
 function SignIn(props) {
     const onClick = () => {
-        const provider = new firebase.auth.PhoneAuthProvider();
-        props.auth.signInWithPopup(provider);
+        const provider = new firebase.auth.GoogleAuthProvider();
+        props.auth.signInWithPopup(provider).then(response => {
+            // create an instance of user
+            // doing so, to find the users
+            const firestore = firebase.firestore();
+            const messageRef = firestore.collection('users');
+
+            const user = response.user;
+            messageRef.doc(user.email).set({
+                name: user.displayName,
+                email: user.email,
+                phone: user.phoneNumber,
+                photoURL: user.photoURL
+            })
+        });
     }
 
     return (<>
