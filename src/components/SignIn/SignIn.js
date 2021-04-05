997c35aba0134afb30da1bf6cafb3ab636e330d6
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2021-04-05 17:51:26 +0530

    api: implements posting user's data in firestore (#5)
    * implements posting user's data in firestore
    
    * fixes typo

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -3,8 +3,21 @@ import firebase from 'firebase/app';
 
 function SignIn(props) {
     const onClick = () => {
-        const provider = new firebase.auth.PhoneAuthProvider();
-        props.auth.signInWithPopup(provider);
+        const provider = new firebase.auth.GoogleAuthProvider();
+        props.auth.signInWithPopup(provider).then(response => {
+            // create an instance of user
+            // doing so, to find the users
+            const firestore = firebase.firestore();
+            const usersRef = firestore.collection('users');
+
+            const user = response.user;
+            usersRef.doc(user.email).set({
+                name: user.displayName,
+                email: user.email,
+                phone: user.phoneNumber,
+                photoURL: user.photoURL
+            })
+        });
     }
 
     return (<>
