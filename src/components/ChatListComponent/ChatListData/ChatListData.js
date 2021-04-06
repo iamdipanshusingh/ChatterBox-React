f3044dbc2451fec5e40dc8025727fd77b845a31f
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2021-04-06 18:33:46 +0530

    implements searching users

--- src/components/ChatListComponent/ChatListData/ChatListData.js
+++ src/components/ChatListComponent/ChatListData/ChatListData.js
@@ -1,12 +1,16 @@
 import classes from './ChatListData.module.scss';
+import Avatar from '../../UI/Avatar/Avatar';
 
 const chatListData = props => {
-    const {message} = props;
+    const { user } = props;
 
     return (
-        <div className={classes.ChatWrapper}>
-            <p className={classes.Title}>{message.name}</p>
-            <p className={classes.Message}>{message.message}</p>
+        <div className={classes.OuterWrapper}>
+            <Avatar avatar={user.photoURL}/>
+            <div className={classes.ChatWrapper}>
+                <p className={classes.Title}>{user.name}</p>
+                <p className={classes.Message}>last message</p>
+            </div>
         </div>
     );
 }
