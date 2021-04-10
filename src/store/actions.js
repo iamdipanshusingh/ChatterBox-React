8d7fefb164813ac23b3ec458d4d9a2875733a7dd
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2021-04-10 16:06:02 +0530

    fixes creating a new chat on clicking a user (#20)

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,3 +1,4 @@
 export const SET_USERS_LIST = 'SET_USERS_LIST';
 export const SELECT_CHAT = 'SELECT_CHAT';
 export const SET_CHATS = 'SET_CHATS';
+export const SET_RECIEVER_CHAT_MAP = 'SET_RECIEVER_CHAT_MAP';
