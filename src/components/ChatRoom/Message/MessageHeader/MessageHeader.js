e38b21de33cc2bc4bc05b2dc084475cb235c1417
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2021-04-11 17:13:52 +0530

    implements signout feature (#27)

--- src/components/ChatRoom/Message/MessageHeader/MessageHeader.js
+++ src/components/ChatRoom/Message/MessageHeader/MessageHeader.js
@@ -1,16 +1,24 @@
 import classes from './MessageHeader.module.scss';
+import React from "react";
 import Avatar from '../../../UI/Avatar/Avatar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 const messageHeader = props => {
-    const { user } = props;
+    const { user, onSignOut } = props;
 
     return (
         <div className={classes.HeaderWrapper}>
-            <Avatar avatar={user.photoURL} />
-            <div className={classes.UserInfoDiv}>
-                <p>{user.name}</p>
-                <span>last seen</span>
+            <div className={classes.IconWrapper}>
+                <FontAwesomeIcon onClick={onSignOut} className={classes.SignOutBtn} icon={faSignOutAlt} />
             </div>
+            {user && <React.Fragment>
+                <Avatar avatar={user.photoURL} />
+                <div className={classes.UserInfoDiv}>
+                    <p>{user.name}</p>
+                    <span>last seen</span>
+                </div>
+            </React.Fragment>}
         </div>
     );
 }
